5554630b273135195c995c14ec199998ac35a7ff
Silvia Saverino <silviasave89@gmail.com>
2023-05-08 10:36:15 +0000

    add cta button to load videoplayer

--- assets/js/scrips.js
+++ assets/js/scrips.js
@@ -1,5 +1,22 @@
 const myButton = document.getElementById("explore");
 
+
+function toggleVideoPlayer() {
+    var videoContainer = document.getElementsByClassName("containerVideo")[0];
+    let ctaButton = document.getElementsByClassName('ctaButton')[0];
+    videoContainer.style.display = "block";
+    ctaButton.style.display= "none";
+  }
+
+function zoomingCTAbutton(){
+    let ctaButton = document.getElementsByClassName('ctaButton')[0];
+    setTimeout(function() {
+        ctaButton.style.opacity = 1;
+        ctaButton.style.zIndex = 6;
+        ctaButton.classList.add('zoom');
+    }, 3000); 
+}
+
 function moveAirplane(){
     const airplane = document.getElementsByClassName('airplane')[0];
     airplane.classList.add('fly');
@@ -19,4 +36,12 @@ function addBeachBackground() {
     }, 3000);
 }
 
+
+function hideExploreButton() {
+    hide=document.getElementById('explore');
+    hide.style.display= 'none';
+}
+
 myButton.addEventListener("click", addBeachBackground);
+myButton.addEventListener("click", hideExploreButton);
+myButton.addEventListener("click", zoomingCTAbutton);
